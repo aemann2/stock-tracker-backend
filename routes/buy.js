0a627c71ea29a7f8ba88569867569ca50a4477fa
aemann2 <aemann2@gmail.com>
2021-11-16 13:49:55 -0800

    added functional POST and PUT requests to /buy route

--- routes/buy.js
+++ routes/buy.js
@@ -10,10 +10,16 @@ router.post('/', auth, async (req, res) => {
 	const userId = req.user.id;
 	const { symbol, shares, price, trans_type } = req.body;
 	try {
-		await db.query(
-			'INSERT INTO transactions (user_id, symbol, shares, price, trans_type, transacted) VALUES ($1, $2, $3, $4, $5, now());',
-			[userId, symbol, shares, price, trans_type]
-		);
+		db.tx(async (t) => {
+			await t.none(
+				'INSERT INTO transactions (user_id, symbol, shares, price, trans_type, transacted) VALUES ($1, $2, $3, $4, $5, now());',
+				[userId, symbol, shares, price, trans_type]
+			);
+			await t.none(
+				'INSERT INTO portfolios (user_id, symbol, shares) VALUES ($1, $2, $3);',
+				[userId, symbol, shares]
+			);
+		});
 		return res.status(200).json({ msg: 'Success' });
 	} catch (err) {
 		console.error(err);
@@ -26,8 +32,23 @@ router.post('/', auth, async (req, res) => {
 // @access private
 router.put('/', auth, async (req, res) => {
 	const userId = req.user.id;
+	const { symbol, shares, price, trans_type } = req.body;
 	try {
-	} catch (err) {}
+		db.tx(async (t) => {
+			await t.none(
+				'INSERT INTO transactions (user_id, symbol, shares, price, trans_type, transacted) VALUES ($1, $2, $3, $4, $5, now());',
+				[userId, symbol, shares, price, trans_type]
+			);
+			await t.none(
+				'UPDATE portfolios SET shares = shares + $1 WHERE symbol = $2 AND user_id = $3;',
+				[shares, symbol, userId]
+			);
+		});
+		return res.status(200).json({ msg: 'Success' });
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({ error: err });
+	}
 });
 
 module.exports = router;
